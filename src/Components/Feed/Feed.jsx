1b85f0add0e5ef9df15c8b6e38a10d44f312cf21
ManeeshaThotakura <maneeshathotakura@gmail.com>
2024-06-03 11:46:59 +0530

    youtube API

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,3 +1,5 @@
+import  { useState,useEffect } from 'react';
+
 import React from 'react'
 import './Feed.css'
 import thumbnail1 from '../../assets/thumbnail1.png'
@@ -9,199 +11,32 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom'
+import {API_KEY} from '../../data'
+const Feed = ({category}) => {
+    const [data,setData] =useState([]);
+    const fetchData = async () =>{
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
+        await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
+    }
+    useEffect(()=>{
+        fetchData();
 
-const Feed = () => {
+    },[category])
+
+
+
   return (
     <div className="feed">
-        <Link to={`video/20/4321`} className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
+        {data.map((item,index)=>{
+            return(
+        <Link to={`video/${item.snippet.categoryid}/${item.id}`} className='card'>
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>{value_converter(item.statistics.viewCount)} views &bull; 2 days ago</p>
         </Link>
-        <div className='card'>
-            <img src={thumbnail2} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Eswar</h3>
-            <p>12k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail3} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>manee</h3>
-            <p>5k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail4} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Chandra</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail5} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Durga</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail6} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>rajini</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail7} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>bhaskar</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail8} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Sri</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail2} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Eswar</h3>
-            <p>12k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail3} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>manee</h3>
-            <p>5k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail4} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Chandra</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail5} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Durga</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail6} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>rajini</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail7} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>bhaskar</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail8} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Sri</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail2} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Eswar</h3>
-            <p>12k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail3} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>manee</h3>
-            <p>5k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail4} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Chandra</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail5} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Durga</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail6} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>rajini</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail7} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>bhaskar</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail8} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Sri</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel for the learning</h2>
-            <h3>Esha</h3>
-            <p>15k views &bull; 2 days ago</p>
-        
-        </div>
+            )
+        })}
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,16 @@
+import  { useState } from 'react';
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Feed from '../../Components/Feed/Feed';
 const Home = ({sidebar}) => {
+
+  const[category,setcategory]= useState(0);
   return (
    <>
-   <Sidebar sidebar={sidebar}/>
+   <Sidebar sidebar={sidebar} category={category} setcategory={setcategory}/>
    <div className={`container ${sidebar?"":'large-container'}`}>
-     <Feed/>
+     <Feed category={category}/>
    
    </div>
    </>
