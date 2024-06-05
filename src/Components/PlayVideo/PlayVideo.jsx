df0e86f7a0cce662f64c1d647e67efc67a424002
ManeeshaThotakura <maneeshathotakura@gmail.com>
2024-06-05 23:50:00 +0530

    youtube API4

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,5 +1,7 @@
+import  { useEffect, useState } from 'react';
 import React from 'react'
 import './PlayVideo.css'
+import {API_KEY, value_converter} from '../../data'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
 import dislike from '../../assets/dislike.png'
@@ -7,15 +9,26 @@ import  share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import moment from 'moment';
 
 const PlayVideo = ({videoId}) => {
+    const [apiData,setApiData]=useState(null);
+    const fetchVideoData = async ()=>{
+        //fetching videos data
+        const videoDetails_url = `https:youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+        await fetch(videoDetails_url).then(res=>res.json()).then(data=>setApiData(data.items[0]));
+    }
+    useEffect(()=>{
+        fetchVideoData();
+ 
+    },[])
   return (
     <div className='play-video'>
        { /*<video src={video1} controls autoPlay muted></video>*/}
-       <iframe  src={`https://www.youtube.com/embed/bSVDUDkEqlw/${videoId}?autoplay=1`}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <h3>Best channel to learn web development </h3>
+       <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <h3>{apiData?apiData.snippet.title:"Title here"}</h3>
         <div className="play-video-info">
-            <p>1556 views &bull; 1day ago</p>
+            <p>{apiData?value_converter(apiData.statistics.viewCount):"16k"} &bull;{ apiData?moment(apiData.snippet.publishedAt).fromNow():""}</p>
             <div>
                 <span><img src={like} alt="" />125</span>
                 <span><img src={dislike} alt="" />25</span>
